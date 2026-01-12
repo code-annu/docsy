f08f106457a2c9734e446b868b98a346dc587a7f
Anurag Rajput <code.annu@gmail.com>
2026-01-12 22:31:16 +0530

    Delete all documents with collaborations on delete user profile

--- backend/src/application/usecases/profile/DeleteProfileUsecase.ts
+++ backend/src/application/usecases/profile/DeleteProfileUsecase.ts
@@ -2,11 +2,17 @@ import { inject, injectable } from "inversify";
 import { TYPES } from "../../../config/types";
 import { IUserRepository } from "../../../domain/repository/IUserRepository";
 import { NotFoundError } from "../../../domain/error/NotFoundError";
+import { ICollaborationRepository } from "@/domain/repository/ICollaborationRepository";
+import { IDocumentRepository } from "@/domain/repository/IDocumentRepository";
 
 @injectable()
 export class DeleteProfileUsecase {
   constructor(
-    @inject(TYPES.IUserRepository) private userRepo: IUserRepository
+    @inject(TYPES.IUserRepository) private userRepo: IUserRepository,
+    @inject(TYPES.IDocumentRepository)
+    private documentRepo: IDocumentRepository,
+    @inject(TYPES.ICollaborationRepository)
+    private collaborationRepo: ICollaborationRepository
   ) {}
 
   async execute(id: string): Promise<void> {
@@ -16,5 +22,10 @@ export class DeleteProfileUsecase {
     }
 
     await this.userRepo.delete(id);
+    const documents = await this.documentRepo.findByOwnerId(id);
+    await this.documentRepo.deleteByOwnerId(id);
+    for (const document of documents) {
+      await this.collaborationRepo.deleteByDocumentId(document.id);
+    }
   }
 }
