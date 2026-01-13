1c367053eb03380b3bb9b16e970536a25664ce63
Anurag Rajput <code.annu@gmail.com>
2026-01-13 13:26:28 +0530

    Implement authentication, profile, dashboard features.

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import { RouterProvider } from "react-router-dom";
+import { appRouter } from "./router";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+
+function App() {
+  return (
+    <Provider store={store}>
+      <RouterProvider router={appRouter} />
+    </Provider>
+  );
+}
+
+export default App;
